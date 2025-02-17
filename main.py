e38d4405dcbc7692a58ecb5d80a671d523b37976
Hirak <hirak170802@gmail.com>
2025-02-17 19:06:30 +0530

    Added data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.textsummarizer.logging import logger
 
 from src.textsummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTraininingPipeline
+from src.textsummarizer.pipeline.stage_2_data_transformtion_pipeline import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = 'Data ingestion stage'
@@ -9,6 +10,18 @@ try:
     logger.info(f"{STAGE_NAME} started")
     pipeline = DataIngestionTraininingPipeline()
     pipeline.run()
+    logger.info(f"{STAGE_NAME} completed")
+except Exception as e:
+    logger.error(f"{STAGE_NAME} failed", exc_info=True)
+    raise e
+
+
+STAGE_NAME = 'Data transformation stage'
+
+try:
+    logger.info(f"{STAGE_NAME} started")
+    pipeline = DataTransformationTrainingPipeline()
+    pipeline.initiate_data_transformation()
     logger.info(f"{STAGE_NAME} completed")
 except Exception as e:
     logger.error(f"{STAGE_NAME} failed", exc_info=True)
